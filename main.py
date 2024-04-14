c974fbc27f8b1ad63cb6d970fa26546a473a058e
Guilherme Fonseca <guiga.froes3@gmail.com>
2024-04-14 14:20:40 -0300

    Df novo e main melhorado

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from flask import Flask, request, url_for, session,redirect
+from flask import Flask, request, url_for, session, redirect, jsonify
 import os
 import requests
 from dotenv import load_dotenv
 import time
 import pandas as pd
-from flask import jsonify
 
 load_dotenv()
 
@@ -14,7 +13,7 @@ client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
 app = Flask(__name__)
- 
+
 app.config['SESSION_COOKIE_NAME'] = 'Spotify Cookie'
 app.secret_key = 'gadiaf76578bfa*5bfaf*'
 TOKEN_INFO = 'token_info'
@@ -24,17 +23,13 @@ def login():
     auth_url = create_spotify_oauth().get_authorize_url()
     return redirect(auth_url)
 
-
-
 @app.route('/redirect')
 def redirect_page():
     session.clear()
     code = request.args.get('code')
     token_info = create_spotify_oauth().get_access_token(code)
     session[TOKEN_INFO] = token_info
-    return redirect(url_for('get_favorite_tracks',external = True))
-
-
+    return redirect(url_for('get_favorite_tracks', external=True))
 
 @app.route('/GetFavoriteTracks')
 def get_favorite_tracks():
@@ -56,19 +51,18 @@ def get_favorite_tracks():
         top_tracks = results['items']
         if not top_tracks:
             break
-        track_ids.extend([track['id'] for track in top_tracks])
-        tracks_info.extend(top_tracks)
-        
-        # Retrieve genres from artists of each track
         for track in top_tracks:
+            track_ids.append(track['id'])
+            tracks_info.append(track)
+            # Retrieve genres from artists of each track
             genres = []
             for artist in track['artists']:
                 artist_details = sp.artist(artist['id'])
                 genres.extend(artist_details['genres'])
             genres_list.append(genres)
 
-    # Fetch audio features for all collected track IDs
-    audio_features = sp.audio_features(track_ids)
+    # Fetch audio features for all collected track IDs in batches
+    audio_features = get_audio_features(sp, track_ids)
     df_audio_features = pd.DataFrame(audio_features)
 
     # Create DataFrame for tracks info
@@ -80,58 +74,38 @@ def get_favorite_tracks():
 
     # Merge DataFrames on 'id'
     df_final = pd.merge(df_tracks, df_audio_features, on='id', how='left')
-    
-    
-    df_final['track_name'] = [track['name'] for track in top_tracks]
-    df_final['track_artist'] = [track['artists'][0]['name'] for track in top_tracks]
-    top_artists_names = df_final['track_artist'].value_counts().head(5)
-    
-    top_artists_info = []
-    for artist_name, count in top_artists_names.items():
-        print(artist_name)
-        results = sp.search(q=  artist_name, type='artist', limit=1)
-        if results['artists']['items']:
-            artist = results['artists']['items'][0]
-            # Extract the URL of the first image for the artist, if available
-            image_url = artist['images'][0]['url'] if artist['images'] else None
-            top_artists_info.append({'artist_name': artist_name, 'image_url': image_url})
 
-    # Create a DataFrame from the top artists info
-    top_artists_df = pd.DataFrame(top_artists_info)
-    print(top_artists_df)
-        
-    # Sai da função top_artists_df, top_artists_names e df com as informações
-    
     df_final.to_csv('df.csv', index=False)
 
-    # For the top artists DataFrame
-    top_artists_df.to_csv('top_artist.csv', index=False)
-    
-    return "Faixas obtidas e analisadas."
+    return "Tracks fetched and analyzed."
 
-   
-   
+def get_audio_features(sp, track_ids):
+    max_ids_per_request = 100
+    audio_features = []
+    for i in range(0, len(track_ids), max_ids_per_request):
+        batch_ids = track_ids[i:i+max_ids_per_request]
+        results = sp.audio_features(batch_ids)
+        audio_features.extend(results)
+    return audio_features
+
 def get_token():
-    token_info = session.get(TOKEN_INFO,None)
+    token_info = session.get(TOKEN_INFO, None)
     if not token_info:
-        redirect(url_for('login',external = False))
-        
+        return redirect(url_for('login', external=False))
+    
     now = int(time.time())
     is_expired = token_info['expires_at'] - now < 30
-    if (is_expired):
+    if is_expired:
         spotify_oauth = create_spotify_oauth()
         token_info = spotify_oauth.refresh_access_token(token_info['refresh_token'])
     return token_info
-    
-
-
 
 def create_spotify_oauth():
     return SpotifyOAuth(
-        client_id= client_id,
-        client_secret= client_secret,
+        client_id=client_id,
+        client_secret=client_secret,
         redirect_uri=url_for('redirect_page', _external=True),
-        scope = "user-library-read playlist-modify-public playlist-modify-private user-top-read"
+        scope="user-library-read playlist-modify-public playlist-modify-private user-top-read"
     )
 
 app.run(debug=True)
